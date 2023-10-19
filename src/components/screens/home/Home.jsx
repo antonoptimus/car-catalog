1ae092f2bc22db5e10e6c447ce22b6808f0dbdcb
Anton Eliseev <anton_saintp@mail.ru>
2023-10-19 16:53:12 +0300

    added image and base styles for card

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,13 +1,26 @@
+import styles from "./Home.module.css";
 
 function Home() {
-   return (
-     <>
-       <h1>
-         Cars catalog
-       </h1>
-     </>
-   )
- }
- 
- export default Home
- 
+  return (
+    <>
+      <h1>Cars catalog</h1>
+      <div>
+        <div className={styles.item}>
+          <div
+            className={styles.image}
+            style={{
+              backgroundImage: "url(/car1.jpg)",
+            }}
+          />
+          <div className={styles.info}>
+            <h2>Car 1</h2>
+            <p>$100 000</p>
+          <button>Read more</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
