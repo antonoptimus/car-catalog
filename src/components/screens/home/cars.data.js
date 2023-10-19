4d37b04e6017cd9c68c05b7966d89d09fc027784
Anton Eliseev <anton_saintp@mail.ru>
2023-10-19 18:56:02 +0300

    output of cards from the local server using map

---
+++ src/components/screens/home/cars.data.js
@@ -0,0 +1,15 @@
+export const cars = [
+   {
+      id: 1,
+      name: 'Toyota Camry',
+      price: 24970,
+      image: 'http://localhost:4200/images/1.jpg'
+   },
+   {
+      id: 2,
+      name: 'Honda Civic',
+      price: 22545,
+      image: 'http://localhost:4200/images/2.jpg'
+   },
+]
+
