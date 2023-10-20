76f4b5d912caf5c590b06f1aa1a35db6f48afcbf
Anton Eliseev <anton_saintp@mail.ru>
2023-10-20 12:44:35 +0300

    added request to server using fetch

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,15 +1,26 @@
 import CarItem from "./car-item/CarItem";
 import CreateCarForm from "./create-car-form/CreateCarForm";
-import {cars as carsData} from "./cars.data";
-import { useState } from "react";
+import { cars as carsData } from "./cars.data";
+import { useEffect, useState } from "react";
 
 function Home() {
-  const [cars, setCars] = useState(carsData)
+  const [cars, setCars] = useState(carsData);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:4200/cars");
+      const data = await response.json();
+
+      setCars(data);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
       <h1>Cars catalog</h1>
-      <CreateCarForm setCars={setCars}/>
+      <CreateCarForm setCars={setCars} />
       <div>
         {cars.length ? (
           cars.map((car) => <CarItem key={car.id} car={car} />)
